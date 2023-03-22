c9338887e1a7c0b75a88173dfd555446222d171f
toaonly <toaonly42@gmail.com>
2023-03-22 13:58:07 +0900

    Update submodules

--- scripts/auto-push.js
+++ scripts/auto-push.js
@@ -3,7 +3,7 @@ import { promiseExec } from "./utils.js";
 try {
   console.log(await promiseExec(`git submodule foreach --recursive git pull`));
 
-  const diff = await promiseExec(`git diff-index --quiet HEAD`);
+  const diff = await promiseExec(`git diff-index HEAD --`);
 
   console.log("diff:", diff);
   console.log("typeof diff:", typeof diff);
