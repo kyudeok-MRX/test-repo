85483efea61a2676ac3e109a2f22b75ce08e4164
toaonly <toaonly42@gmail.com>
2023-03-22 13:56:24 +0900

    test

---
+++ scripts/auto-push.js
@@ -0,0 +1,19 @@
+import { promiseExec } from "./utils.js";
+
+try {
+  console.log(await promiseExec(`git submodule foreach --recursive git pull`));
+
+  const diff = await promiseExec(`git diff-index --quiet HEAD`);
+
+  console.log("diff:", diff);
+  console.log("typeof diff:", typeof diff);
+  console.log("diff.length:", diff.length);
+
+  if (!diff) console.log(`No changes to commit`);
+  else {
+    await promiseExec(`git add .`);
+    await promiseExec(`git commit -am "Update submodules"`);
+  }
+} catch (error) {
+  console.warn(error.message);
+}
